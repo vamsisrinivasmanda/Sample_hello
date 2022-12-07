079dc87377153e0259809b72c6438e2da69df691
vamsisrinivasmanda <vamsisrinivasmanda@gmail.com>
2022-12-07 08:45:39 +0530

    first commit

---
+++ src/CashWithdrawal/index.js
@@ -0,0 +1,19 @@
+import {Component} from 'react'
+import './index.css'
+
+class CashWithdrawal extends Component {
+  render() {
+    return (
+      <div className="bg-container">
+        <div className="card">
+          <div className="name-container">
+            <div>s</div>
+            <p>Sarah Williams</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default CashWithdrawal
